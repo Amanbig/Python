63f4430a08dcca4e5d41ee2bf10e98925897658d
Amanbig <93067432+Amanbig@users.noreply.github.com>
2022-04-23 11:01:27 +0530

    In  this program we can swap two numbers using third number

---
+++ swap.py
@@ -0,0 +1,7 @@
+a=int(input("Enter number\n"));
+b=int(input("Enter another number\n"));
+print(f"Before swapping a={a} and b={b}\n");
+c=int(a);
+a=b;
+b=c;
+print(f"After swapping a={a} and b={b}\n");
